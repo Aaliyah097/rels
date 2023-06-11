e189dfa806af86cc6dfbc01b48468a876c105228
Aaliyah097 <name.boltz@gmail.com>
2023-06-11 17:22:58 +0300

    main

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from src.graph_db.repository import (
     get_nodes_and_links
 )
 from src.server import app
+import sys
 
 
 __doc__ = "enter point"
 
 
 if __name__ == '__main__':
-    app.app.run()
+    if 'prod' in sys.argv[1:]:
+        app.app.run(host='0.0.0.0')
+    else:
+        app.app.run()
